4b95ec34113151ecd0e66d7de53366366f4dbd80
Allen <a1b2c3d42003hongkong@hotmail.com>
2016-01-12 18:35:02 +0800

    remove backup code

--- translate.sh
+++ translate.sh
@@ -12,19 +12,3 @@ function init(){
 }
 setTimeout(init,0);
 END_OF_FILE
-
-exit 1
-echo -n 'replayerBody="' > youtube.js
-sed -e "/\/\//d" < src/youtube.js |  tr -d "\t\r\n" >> youtube.js
-echo '";' >> youtube.js
-echo "function init(){" >> youtube.js
-echo "		var script = document.createElement('script');" >> youtube.js
-echo "		script.innerHTML = replayerBody+';if(!(\"\$\" in window)){$=function(a){return document.querySelector(a)}};setInterval(Replayer.init.main,2000);';" >> youtube.js
-echo "		try{" >> youtube.js
-echo "			document.querySelector('head').appendChild(script);" >> youtube.js
-echo "		}catch(newE){" >> youtube.js
-echo "			console.log('Error Occur when initializing Replayer: ', newE);" >> youtube.js
-echo "			return;" >> youtube.js
-echo "		}" >> youtube.js
-echo "}" >> youtube.js
-echo "setTimeout(init,0);" >> youtube.js
