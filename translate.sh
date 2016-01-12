16185a8040a0214afd6b94cfc45cfc6f411ddb83
Allen <a1b2c3d42003hongkong@hotmail.com>
2016-01-12 18:34:03 +0800

    update translate.sh to more readable version

--- translate.sh
+++ translate.sh
@@ -1,3 +1,19 @@
+MAIN_PROG=`sed -e "/\/\//d" < src/youtube.js |  tr -d "\t\r\n"`
+
+cat > youtube.js <<END_OF_FILE
+function init(){
+	var script = document.createElement('script');
+	script.innerHTML = "${MAIN_PROG};if(!('\$' in window)){$=function(a){return document.querySelector(a)}};setInterval(Replayer.init.main,2000);";
+	try{
+		document.querySelector('head').appendChild(script);
+	}catch(newE){
+		console.log('Error Occur when initializing Replayer: ', newE);
+	}
+}
+setTimeout(init,0);
+END_OF_FILE
+
+exit 1
 echo -n 'replayerBody="' > youtube.js
 sed -e "/\/\//d" < src/youtube.js |  tr -d "\t\r\n" >> youtube.js
 echo '";' >> youtube.js
